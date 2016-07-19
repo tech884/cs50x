496de9dd44e8077db56b151dfaced94b55a48b6b
tech884 <tech884@users.noreply.github.com>
2016-07-19 11:45:13 -0700

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * hello.c
+ *
+ * Eric L
+ *
+ * Outputs "hello, world" to standard output device in the session.
+ */
+ 
+ #include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world\n");
+}
