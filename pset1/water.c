95665c4e384dcd72c082ee4cb775826f46fca609
tech884 <tech884@users.noreply.github.com>
2016-07-19 11:59:13 -0700

    Create water.c

---
+++ pset1/water.c
@@ -0,0 +1,22 @@
+/**
+ * water.c
+ *
+ * Eric L
+ *
+ * Takes user input of shower length and outputs equivalent volume of water bottles.
+ */
+ 
+#include <stdio.h>
+#include <cs50.h>
+
+int main()
+{
+    // prompt user for shower length
+    printf("How long is your shower to the nearest whole minute?: ");
+    int shower_length = GetInt();
+    
+    // convert shower length to number of water bottles
+    int num_of_bottles = 12 * shower_length;
+    
+    printf("That's equivalent to the following number of 16-oz water bottles: %i\n", num_of_bottles);
+}
