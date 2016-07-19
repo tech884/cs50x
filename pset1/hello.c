df76d81ce2a8c995a82ca9bb81f3f01a2f3c0021
tech884 <tech884@users.noreply.github.com>
2016-07-19 12:02:25 -0700

    Update hello.c

--- pset1/hello.c
+++ pset1/hello.c
@@ -1,10 +1,10 @@
 /**
- * hello.c
- *
- * Eric L
- *
- * Outputs "hello, world" to standard output device in the session.
- */
+ Name: hello.c
+ 
+ Eric L
+ 
+ Outputs "hello, world" to standard output device in the session.
+*/
  
 #include <stdio.h>
 
